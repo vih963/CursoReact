2151503085c66fe080f613f9d6df163e08f399f2
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-06-26 21:42:49 -0300

    ReactComponents

---
+++ Aulas/AULA 2 FUNDAMENTOS/fundamentos1/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import FirstComponent from "./components/FistComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Aulas/AULA 2 FUNDAMENTOS/fundamentos1/src/components/FistComponent.tsx
@@ -0,0 +1,10 @@
+const FirstComponent = () => {
+    return(
+        <div>
+            <h1>Meu primeiro Componente</h1>
+        </div>
+    )
+
+}
+
+export default FirstComponent;
