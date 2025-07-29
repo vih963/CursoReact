310d20d1b765fe6069eae05365e6c7c4edfeff8f
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-07-28 23:14:01 -0300

    React

--- Aulas/ex03/excontador/src/App.tsx
+++ Aulas/ex03/excontador/src/App.tsx
@@ -4,27 +4,34 @@ import { MyComponent } from './components/MyComponentes'
 import { SegundoComponent } from './components/SegundoComponent'
 import { Events } from './components/Event'
 import Challenge2 from './components/Challenge2'
-import  HooksEmUso from './components/HooksEmUso' 
+import HooksEmUso from './components/HooksEmUso'
 import ListaRenderizada from './components/ListaRenderizada'
 import CondicionalRender from './components/CondicionalRender'
+import ShowUserName from './components/ShowUserName'
+import { useState } from 'react'
 
 
 
 function App() {
-    return (
+
+  const name = "Joaquim";
+  const [userName] = useState("Maria")
+
+  return (
     <div>
       <h1>Aqui vai ser o contador</h1>
-    <Botao />
-    <SegundoComponent />
-    <MyComponent />
-    <Events />
-    <Challenge2 />
-    <HooksEmUso />
-    <ListaRenderizada />
-    <CondicionalRender />
-      
+      <Botao />
+      <SegundoComponent />
+      <MyComponent />
+      <Events />
+      <Challenge2 />
+      <HooksEmUso />
+      <ListaRenderizada />
+      <CondicionalRender />
+      <ShowUserName name={userName} />
+
     </div>
-    
+
   )
 }
 
--- Aulas/ex03/excontador/src/components/CondicionalRender.tsx
+++ Aulas/ex03/excontador/src/components/CondicionalRender.tsx
@@ -1,13 +1,26 @@
 import { useState } from "react"
 
 const CondicionalRender = () => {
-    const [x] = useState(false)
+  const [x] = useState(false)
+
+  const [name, setName] = useState("Vinicius")
 
   return (
     <div>
-        <h1>Isso será exibido?</h1>
-        {x && <p> se X for true, sim</p>}
-        {!x && <p> se X for false, aparece esse </p>}
+      <h1>Isso será exibido?</h1>
+      {x && <p> se X for true, sim</p>}
+      {!x && <p> se X for false, aparece esse </p>}
+      <h1>IF ternario</h1>
+      {name === "João" ? (
+        <div>
+          <p>O nome é João</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado</p>
+        </div>
+      )}
+      <button onClick={() => setName("João")}> Clique aqui</button>
     </div>
   )
 }
