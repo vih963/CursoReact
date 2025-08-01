7cb131e5d102c9b86a48e3396d7eb5a8bb03e0c0
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-07-31 21:49:04 -0300

    React

---
+++ Aulas/HeroCode/react-vite/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export function Navbar() {
+    return (
+        <nav>
+            <div>
+                <h1>Navbar</h1>
+            </div>
+        </nav>
+    )
+}
---
+++ Aulas/HeroCode/react-vite/src/components/Section.tsx
@@ -0,0 +1,8 @@
+export function Section() {
+    return (
+        <section>
+            <h1>section 1</h1>
+            <h2>section 2</h2>
+        </section>
+    )
+}
